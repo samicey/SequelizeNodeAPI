82a2a5876d72865cf26c93683bb1b5d11a16aebe
Obikaonu Samuel <samuelchibuike22@gmail.com>
2020-02-12 20:57:49 +0100

    all

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -28,9 +28,10 @@ module.exports = {
 
   production: {
     database: process.env.DB_NAME,
-    username: process.env.DB_USER,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
+    port:5432,
     dialect: 'postgres'
   }
 };
