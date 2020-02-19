f992502cd86ad0da91036fc02e4bf01e17c31415
OBIKAONU SAMUEL <49449271+samicey@users.noreply.github.com>
2020-02-19 16:50:38 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import config from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
-import bookRoutes from './server/routes/BookRoutes';
+import bookRtes from './server/routes/BookRoutes';
 import cors from 'cors';
 
 config.config();
